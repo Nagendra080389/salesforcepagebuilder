e8faf4e9e6c84163dad9a6323650fe9aaace8f1b
nagesingh@deloitte.com <nagesingh@deloitte.com>
2019-10-26 20:23:49 +0530

    angular js and jquery

--- src/main/java/com/sfpage/salesforcepagebuilder/SecurityConfig.java
+++ src/main/java/com/sfpage/salesforcepagebuilder/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.sfpage.salesforcepagebuilder;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,10 +11,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        //...
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .antMatchers("/**").permitAll();
+        http.authorizeRequests().antMatchers("/").permitAll();
     }
 }
