7d31a709b3b01d31832198bb3459c57772f06a97
nagesingh@deloitte.com <nagesingh@deloitte.com>
2019-10-26 21:29:01 +0530

    angular js and jquery

--- src/main/java/com/sfpage/salesforcepagebuilder/SecurityConfig.java
+++ src/main/java/com/sfpage/salesforcepagebuilder/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +20,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated()
+        http/* Allow the app to show in a frame */
+                .headers().frameOptions().disable()
+                .and()
+                /* always create a server session */
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .and()
+                /*
+                 * Define the URLs that can be accesses without authentication
+                 */
+                .authorizeRequests()
+                .antMatchers("/",
+                        "/sfdcauth/**")
+                .permitAll()
+                /* Require all others to be authenticated */
+                .anyRequest().authenticated()
                 .and()
                 /* Add the filter that turns JWT into authentication */
                 .addFilter(new CanvasAuthorizationFilter(this.authenticationManager()))
